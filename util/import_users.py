0c9a7dda4d27de79c945b3124ca3e3df8cfb3893
ailton <ailton.xavier@fomento.to.gov.br>
2024-08-30 13:59:54 -0300

    importe usuarios

--- util/import_users.py
+++ util/import_users.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import pytz
+from fast_zero.models.models import User
 import sqlalchemy as sa
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -23,8 +26,8 @@ engine2 = create_engine(DATABASE_URL2)
 
 
 def consultaUsers():
-    select_user_sql = """SELECT id, username, "password", email,
-    is_active, date_joined, first_name, last_name, last_login, is_superuser, is_staff, 
+    select_user_sql = """SELECT id, username, password, email,
+    is_active, date_joined, first_name, last_name, last_login, is_superuser, is_staff 
     FROM public.auth_user ORDER BY id;"""
 
     with engine.connect() as connection:
@@ -34,7 +37,7 @@ def consultaUsers():
         # Imprima os resultados
         for linha in resultado:
             print(linha)
-            #insertUser(linha)
+            insertUser(linha)
 
 
 def insertUser(row):
@@ -65,38 +68,43 @@ def insertUser(row):
 
     # Substitui None por valores apropriados para SQL
     row = tuple('' if x is None else x for x in row)
+    TIME_ZONE = 'America/Sao_Paulo'
+    tz = pytz.timezone(TIME_ZONE)
 
     with engine2.connect() as connection:
         transaction = connection.begin()
-        try:
-            # Execute o comando SQL com a ligação de parâmetros
-            resultado = connection.execute(
-                sa.text(insert_sql),
-                {
-                    'id':row[0],
-                    'username':row[1],
-                    'password':row[2],
-                    'email':row[3],
-                    'is_active':row[4],
-                    'created_at':row[5],
-                    'updated_at':None,
-                    'full_name':row[0],
-                    'otp_base32':row[0],
-                    'otp_created_at':row[0],
-                    'login_otp_used':row[0],
-                    'is_staff':row[0],
-                    'is_superuser':row[0],
-                    'otp_auth_url':row[0],
-                    'last_login': row[8]
-                },
-            )
-            print(resultado)
-            transaction.commit()
-        except Exception as e:
-            transaction.rollback()
-            print(f'Erro ao inserir negociação de crédito: {e}')
-            raise
+        db_row = connection.execute(sa.text(f"SELECT id FROM public.users where id ={row[0]}"))
+        result = db_row.fetchone()
+        if not result:
+            try:
+                # Execute o comando SQL com a ligação de parâmetros
+                resultado = connection.execute(
+                    sa.text(insert_sql),
+                    {
+                        'id':row[0],
+                        'username':row[1],
+                        'password':row[2],
+                        'email':row[3] if row[3] else row[1]+'@fomento.to.gov.br',
+                        'is_active':row[4],
+                        'created_at':row[5],
+                        'updated_at':datetime.now(tz),
+                        'full_name':row[6]+' '+row[7],
+                        'otp_base32':User.create_otp_base32(),
+                        'otp_created_at':datetime.now(tz),
+                        'login_otp_used':False,
+                        'is_staff':row[10],
+                        'is_superuser':row[9],
+                        'otp_auth_url':'',
+                        'last_login': row[8]
+                    },
+                )
+                print(resultado)
+                transaction.commit()
+            except Exception as e:
+                transaction.rollback()
+                print(f'Erro ao inserir negociação de crédito: {e}')
+                raise
 
 
 if __name__ == '__main__':
-    consultaUser()
+    consultaUsers()
